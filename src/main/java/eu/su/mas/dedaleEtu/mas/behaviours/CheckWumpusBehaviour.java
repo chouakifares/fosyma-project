a3ca4dab1ba6a0cf70ae0560d0c12f7406352eb8
Kahil-amine <kahil.amine@outlook.fr>
2022-04-27 10:26:38 +0200

    Added a behaviour to check if the blocked nodes are still blocked

--- src/main/java/eu/su/mas/dedaleEtu/mas/behaviours/CheckWumpusBehaviour.java
+++ src/main/java/eu/su/mas/dedaleEtu/mas/behaviours/CheckWumpusBehaviour.java
@@ -1,12 +1,24 @@
 package eu.su.mas.dedaleEtu.mas.behaviours;
 
 import eu.su.mas.dedale.mas.AbstractDedaleAgent;
+import eu.su.mas.dedaleEtu.mas.agents.dummies.explo.BaseExplorerAgent;
+import eu.su.mas.dedaleEtu.mas.knowledge.MapRepresentation;
 import jade.core.behaviours.WakerBehaviour;
+
+import java.util.ArrayList;
 
 public class CheckWumpusBehaviour extends WakerBehaviour {
 
 
     public CheckWumpusBehaviour(AbstractDedaleAgent a, long timeout){
         super(a, timeout);
+    }
+
+    @Override
+    protected void onWake() {
+        // Retrieve the list of blocked nodes
+        MapRepresentation myMap = ((BaseExplorerAgent)myAgent).getMap();
+        List<String> blockedNodes = new ArrayList<String>();
+        myMap.getGraph().nodes().iterator();
     }
 }
